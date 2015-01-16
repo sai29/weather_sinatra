3c91774c80436d64511b9a50f7f7c4ab54b242f6
sai29 <saiprashanth93@gmail.com>
2015-01-16 23:34:49 +0530

    First version without barometer

--- app.rb
+++ app.rb
@@ -1,13 +1,38 @@
 require 'sinatra'
-require "barometer"
+
+require 'open-uri'
+require 'json'
 get '/' do
-  erb :home
+	erb :home
 end
 
 get '/:city' do
-  @city = params[:city]
-  barometer = Barometer.new @city
-  @weather = barometer.measure
-  erb :weather
+	city_name = params[:city].chomp.split
+	city_name.each do |val|
+		val.capitalize!
+	end
+	city_name = city_name.join("_")
+	puts city_name
+	wunderground_url_current = 'http://api.wunderground.com/api/31c52a5653ebf884/geolookup/conditions/q/' + city_name + '.json'
+	open(wunderground_url_current) do |f|
+		json_string = f.read
+		json_response = JSON.parse(json_string)
+		@city = json_response['location']['city']
+		@current_temp = json_response['current_observation']['temp_c']
+		@weather = json_response['current_observation']['weather']
+		@relative_humidity = json_response['current_observation']['relative_humidity']
+	end
+
+	wunderground_url_forecast = 'http://api.wunderground.com/api/31c52a5653ebf884/geolookup/forecast/q/' + city_name + '.json'
+	open(wunderground_url_forecast) do |f|
+		json_string = f.read
+		json_response = JSON.parse(json_string)
+		@forecast = json_response['forecast']['simpleforecast']['forecastday']
+		@forecast_array = []
+		@forecast.each do |var|
+			@forecast_array << [var['high']['celsius'],var['low']['celsius']]
+		end
+	end
+	erb :weather
 end
 
